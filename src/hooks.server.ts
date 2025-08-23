72a427f32c25e555372fe72e28ae4c96245c1574
Guillaume Bielli <guillaume.bielli@gmail.com>
2025-08-23 15:20:50 +0200

    protected routes

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,11 @@
 import { building } from '$app/environment';
 import { auth } from '$lib/server/auth';
 import type { Handle } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 import { svelteKitHandler } from 'better-auth/svelte-kit';
+
+// Configuration des routes protégées
+const PROTECTED_ROUTES = ['/dashboard', '/profile'];
 
 export const handle: Handle = async ({ event, resolve }) => {
 	// Fetch current session from Better Auth
@@ -13,6 +17,12 @@ export const handle: Handle = async ({ event, resolve }) => {
 	if (session) {
 		event.locals.session = session.session;
 		event.locals.user = session.user;
+	}
+
+	if (!session) {
+		if (PROTECTED_ROUTES.includes(event.url.pathname)) {
+			return redirect(302, '/auth/login');
+		}
 	}
 
 	return svelteKitHandler({ event, resolve, auth, building });
